96371601a43a40d1a3a6dc97b2f39d2ad2c0211d
Ashrafshwayat <115452472+Ashrafshwayat@users.noreply.github.com>
2023-01-31 20:59:03 +0300

    ashrffff

---
+++ MVC-2023/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_2023.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        //public string Index() 
+        // {
+        //     return "<a href='https://localhost:44363/Default/DownloadImage'><img style=\"width :50px\" src='img/el-salvador.png'/></a>"; 
+
+        // }
+
+
+
+
+        //   public ActionResult DownloadImage()
+        //{
+        //    var filePath = Server.MapPath("~/img/el-salvador.png");
+        //    return File(filePath,"png","el-salvador");
+        //}
+
+        public string Index() {
+
+            return ("<a download  href='../img/el-salvador.png'><img src='../img/el-salvador.png'/></a>");
+        }
+        public string About()
+        {
+            return "About";
+        }
+        public string coun()
+        {
+            return "coun";
+        }
+    }
+
+}
